68153d9d93502e4a7fecac649d1f636c01ebbaf4
BKIslem <benkhaled.islem3@gmail.com>
2023-05-01 15:10:35 +0200

    commit corrigé5

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,5 +3,26 @@ function ModeDark() {
     var element = document.getElementById('id-body');
     element.classList.toggle('dark');
 }
+let sentences = document.querySelectorAll('.sentence');
+let characterCount = 0;
+
+for (let i = 0; i < sentences.length; i++) {
+  let sentence = sentences[i];
+  let newContent = '';
+
+  // go through all characters of the sentence
+  for (let j = 0; j < sentence.textContent.length; j++) {
+    let substring = sentence.textContent.substr(j, 1);
+    // if we have a character, wrap it
+    if (substring !== " ") {
+      newContent += `<span style="--animation-order: ${characterCount};">${substring}</span>`;
+    } else {
+      newContent += substring;
+    }
+    characterCount++;
+  }
+  sentence.innerHTML = newContent;
+}
+
 
    
